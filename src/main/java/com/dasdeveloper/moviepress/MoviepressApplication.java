1513b55a90ac4167ccf2719cced20b2e8a262051
DasDeveloper <rohandas@live.ca>
2023-04-29 15:20:18 -0400

    Setup

---
+++ src/main/java/com/dasdeveloper/moviepress/MoviepressApplication.java
@@ -0,0 +1,13 @@
+package com.dasdeveloper.moviepress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviepressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviepressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dasdeveloper/moviepress/MoviepressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dasdeveloper.moviepress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviepressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
