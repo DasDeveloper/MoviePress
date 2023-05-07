f0ca60a4f7f5a32079241d8d9fa3c24063ab9668
DasDeveloper <rohandas@live.ca>
2023-05-07 17:54:19 -0400

    changes

--- client/src/pages/signup.jsx
+++ client/src/pages/signup.jsx
@@ -1,13 +1,20 @@
 import { useState } from "react";
 import "../css/signup.css"
+import axios from "axios"
 
 const Signup = () =>{
 
     const [firstName, setFirstName] = useState();
     const [lastName, setLastName] = useState();
     const [email, setEmail] = useState();
-    const [password,, setPassword] = useState();
+    const [password, setPassword] = useState();
     const [role, setRole] = useState("User");
+
+    const onSubmit = () => {
+
+        const User = axios.get(`http://localhost:8080/api/users/check/${email}`)
+        console.log(User)
+    }
 
     return(
         <div>
@@ -19,13 +26,13 @@ const Signup = () =>{
                 </div>
                 
                 <label for="firstname">First Name</label>
-                <input placeholder="Enter first name" name="firstname"/>
+                <input placeholder="Enter first name" onChange={e => setFirstName(e.target.value)} name="firstname"/>
                 <label for="lastname">Last Name</label>
-                <input placeholder="Enter last name" name="lastname"/>
+                <input placeholder="Enter last name" onChange={e => setLastName(e.target.value)} name="lastname"/>
                 <label for="email">Email</label>
-                <input placeholder="Enter email" name="email"/>
+                <input placeholder="Enter email" onChange={e => setEmail(e.target.value)} name="email"/>
                 <label for="password">Password</label>
-                <input placeholder="Enter password" type="password" name="password"/>
+                <input placeholder="Enter password" onChange={e => setPassword(e.target.value)} type="password" name="password"/>
 
                 <button> Sign Up</button>
 
