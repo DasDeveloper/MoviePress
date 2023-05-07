402694e1cfb009b2bb06d900edbe143f25a52b42
DasDeveloper <rohandas@live.ca>
2023-05-06 22:53:32 -0400

    Update signup.jsx

--- client/src/pages/signup.jsx
+++ client/src/pages/signup.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import "../css/signup.css"
 
 const Signup = () =>{
+
+    const [firstName, setFirstName] = useState();
+    const [lastName, setLastName] = useState();
+    const [email, setEmail] = useState();
+    const [password,, setPassword] = useState();
+    const [role, setRole] = useState("User");
 
     return(
         <div>
